d1fc950e9ffe5486c4ce3380d8466c574ab64144
semturk@gmail.com <semturk@gmail.com@97140a43-7a26-d996-ddd1-67d16507f70c>
2012-05-21 13:18:59 +0000

    New Test for service constructor added.

--- trunk/com.soda.app/test/com/soda/app/ServiceTest.java
+++ trunk/com.soda.app/test/com/soda/app/ServiceTest.java
@@ -3,15 +3,26 @@ package com.soda.app;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+//TEST
 public class ServiceTest 
 {
 
+	public final static int SERVICE_ID = -5;
+	
 	@Test
 	public void testSetServiceID() 
 	{
-		Service tester = new Service();
-		assertEquals("Result", false, tester.setServiceID(-5));
+		Service tester = new Service(SERVICE_ID);
+		assertEquals("Result", true, tester.setServiceID(SERVICE_ID));
+		
+		
+	}
+	
+	@Test
+	public void testConstructor() 
+	{
+		Service tester = new Service(SERVICE_ID);
+		assertFalse(tester.getServiceID()==0);
 	}
 
 }
